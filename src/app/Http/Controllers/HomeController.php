8d884cf047dd41d8ef7ec1ffd02bd33fc454c6ca
ibu-kimura <ibu-kimura@sod.co.jp>
2023-02-17 17:58:09 +0900

    add search

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\WebResults;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
@@ -23,7 +22,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
         $list = 
         DB::table('web_results')
@@ -32,19 +31,45 @@ class HomeController extends Controller
         ->select('web_results.*','search_words.keyword','target_services.site_name')
         ->orderby('created','desc')
         ->paginate(100);
+        $target_list=
+        DB::table('target_services')
+        ->orderby('site_name','asc')
+        ->get();
 
-        if(isset($_GET['sort'])){
+
+        //sort
+        if(isset($request->sort)){
             $list = 
             DB::table('web_results')
             ->leftjoin('search_words', 'search_words.id', '=', 'web_results.search_word_id')
             ->leftjoin('target_services', 'target_services.id', '=', 'web_results.target_id')
             ->select('web_results.*','search_words.keyword','target_services.site_name')
-            ->orderby($_GET['sort'],$_GET['direction'])
+            ->orderby($request->sort,$request->direction)
             ->paginate(100);
         }
-        $target_list=
-        DB::table('target_services')
-        ->get();
+
+        //search
+        if(isset($request->target_id)){
+            $list = 
+            DB::table('web_results')
+            ->leftjoin('search_words', 'search_words.id', '=', 'web_results.search_word_id')
+            ->leftjoin('target_services', 'target_services.id', '=', 'web_results.target_id')
+            ->select('web_results.*','search_words.keyword','target_services.site_name')
+            ->where('target_id',$request->target_id)
+            ->orderby('created','desc')
+            ->paginate(100);
+        }
+        if(isset($request->search_keyword)){
+            $list = 
+            DB::table('web_results')
+            ->leftjoin('search_words', 'search_words.id', '=', 'web_results.search_word_id')
+            ->leftjoin('target_services', 'target_services.id', '=', 'web_results.target_id')
+            ->select('web_results.*','search_words.keyword','target_services.site_name')
+            ->where('site_name','like',"%".$request->search_keyword."%")
+            ->orderby('created','desc')
+            ->paginate(100);
+        }
+
         return view('home',['list' => $list,'target_list' => $target_list]);
     }
 }
